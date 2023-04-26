6b503602b865e084a878969c9f4ee51eb8779e3a
Beste Kocaoğlu <bestekocaoglu@hotmail.com>
2023-04-26 19:28:44 +0300

    Initial Commit

---
+++ MachineLearningImageRecognition/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MachineLearningImageRecognition
+//
+//  Created by Beste Kocaoglu on 26.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
